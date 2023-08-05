834162f7a2303cc7a232f97e8fab5e31caae30f0
VID Neto <dello5@yandex.ru>
2023-08-05 23:19:53 +0300

    all done

--- src/test/java/ru/netology/patterns_2/test/AuthTest.java
+++ src/test/java/ru/netology/patterns_2/test/AuthTest.java
@@ -77,6 +77,7 @@ class AuthTest {
         var wrongPassword = getRandomPassword();
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(wrongPassword);
+        $("button.button").click();
         $("[data-test-id='error-notification'] .notification__content")
                 .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
                 .shouldBe((Condition.visible));
